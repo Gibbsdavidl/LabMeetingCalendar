39705f7007df58619be20967c98da0396642b0fb
David L Gibbs <gibbsdavidl@gmail.com>
2016-10-21 12:39:31 -0700

    update to calendar

--- src/genList.py
+++ src/genList.py
@@ -16,7 +16,7 @@ emails = open("dat/emailList.txt",'r').read().strip().split("\n")
 en = len(emails) # number of speakers
 idx = 0          # speaker index mod en
 
-holidays = [(1,1), (1,18), (2,15), (5,30), (7,4), (8,5), (11,24), (11,25), (12,25), (12,26)]
+holidays = [(1,1), (1,18), (2,15), (5,30), (7,4), (9,5), (11,24), (11,25), (12,25), (12,26)]
 
 # The permutation #
 np.random.seed()
@@ -28,7 +28,7 @@ for yi in [2016,2017]:
     # for each month
     for mi in [11,12,1,2,3,4,5,6,7,8,9,10,11,12]:
         for di in cal.itermonthdays2(2016, mi): # iterate over days, Sunday == 0, Thursday == 4
-            if di[1] == 3 and di[0] != 0 and di not in holidays:
+            if (di[1] == 3) and (di[0] != 0) and ((mi,di[0]) not in holidays):
                 p = emails[emailPerm[idx % en]].split(',')
-                print("\t".join([str(mi), str(di[0]), p[2], p[1]]))
+                print("\t".join([str(yi), str(mi), str(di[0]), p[2], p[1]]))
                 idx +=1 
